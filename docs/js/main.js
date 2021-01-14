4fd5f8618b6c2f8dab49a413b7212746ae88b65d
Alexey.Strigalev <Steelrat83@gmail.com>
2021-01-14 18:25:04 +0400

    download project

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.strategies-card-row').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        speed: 1000,
+      });
+
+})
